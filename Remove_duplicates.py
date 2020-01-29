c417f81c78bbe15fa196ff15e57728e820d27eb0
pritamhash <pritamhash@gmail.com>
2020-01-29 12:12:08 +0530

    Adding a python algo file This was done with sublime text editor

---
+++ Remove_duplicates.py
@@ -0,0 +1,20 @@
+# Python3 program to remove duplcates 
+def removeDulipcates(arr): 
+    s = dict() 
+    d = [] 
+  
+    for i in arr: 
+        if i not in s.keys(): 
+              
+            # Adding elements to the dictionary 
+            d.append(i) 
+            s[i] = 1
+      
+    # Printing the dictionary 
+    print(d) 
+  
+# Driver Code 
+arr = [1, 2, 5, 1, 7, 2, 4, 2] 
+removeDulipcates(arr)      
+  
+# This code is contributed 
